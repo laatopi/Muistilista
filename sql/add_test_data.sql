3a7f554c857716d02023eb57d52102b87b70ca3a
laatopi <laantop@gmail.com>
2017-03-23 22:39:43 +0200

    Viikon 2 Deadline teot

--- sql/add_test_data.sql
+++ sql/add_test_data.sql
@@ -1,1 +1,10 @@
 -- Lisää INSERT INTO lauseet tähän tiedostoon
+INSERT INTO Player (name, password) VALUES ('Henri', 'Henri123');
+INSERT INTO Kayttaja (tunnus, salasana) VALUES ('Taneli','salasana');
+INSERT INTO Tehtava (nimi, lisayspaiva, tarkeysaste, deadline, kayttaja_id) VALUES ('Pyykkien pesu', '2013-03-27', 3, '2013-03-30', 1);
+INSERT INTO Luokka (nimi) VALUES ('Kotityöt');
+INSERT INTO Luokka (nimi) VALUES ('Hygienia');
+
+INSERT INTO LuokkaApuTaulukko (tehtava_id, luokka_id) VALUES (1, 1);
+
+INSERT INTO AlaluokkaApuTaulukko(ylaluokka_id, alaluokka_id) VALUES (1, 2);
--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,1 +1,32 @@
--- Lisää CREATE TABLE lauseet tähän tiedostoon
+CREATE TABLE Kayttaja(
+  kayttaja_id SERIAL PRIMARY KEY,
+  tunnus varchar (15) NOT NULL,
+  salasana varchar (10) NOT NULL
+);
+
+CREATE TABLE Tehtava(
+  tehtava_id SERIAL PRIMARY KEY,
+  nimi varchar(50) NOT NULL, 
+  lisayspaiva DATE, NOT NULL
+  tarkeyasaste INTEGER, NOT NULL
+  deadline DATE,
+  kayttaja_id INTEGER REFERENCES Kayttaja(kayttaja_id)
+);
+
+CREATE TABLE Luokka(
+  luokka_id SERIAL PRIMARY KEY,
+  nimi varchar(50) NOT NULL,
+  ylaluokka  NOT NULL
+);
+
+CREATE TABLE Player(
+  id SERIAL PRIMARY KEY,
+  name varchar(50) NOT NULL,
+  password varchar(50) NOT NULL
+);
+
+CREATE TABLE Player(
+  id SERIAL PRIMARY KEY, -- SERIAL tyyppinen pääavain pitää huolen, että tauluun lisätyllä rivillä on aina uniikki pääavain. Kätevää!
+  name varchar(50) NOT NULL, -- Muista erottaa sarakkeiden määrittelyt pilkulla!
+  password varchar(50) NOT NULL
+);
